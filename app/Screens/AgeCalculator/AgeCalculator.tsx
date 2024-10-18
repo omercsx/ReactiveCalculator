03db7b42d1447898c603b36544c5295d4644a223
omercsx <omercagris@gmail.com>
2024-10-18 15:59:25 -0400

    age calculator

--- app/Screens/AgeCalculator/AgeCalculator.tsx
+++ app/Screens/AgeCalculator/AgeCalculator.tsx
@@ -1,13 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import DateTimePicker from 'react-native-ui-datepicker';
+import dayjs from 'dayjs';
+import { useState } from 'react';
 
 const AgeCalculator = () => {
+	const [date, setDate] = useState(dayjs());
+
+	const calculateAge = () => {
+		const age = dayjs().diff(date, 'year');
+		alert(`You are ${age} years old`);
+	};
+
 	return (
-		<View>
-			<Text>AgeCalculator</Text>
+		<View style={styles.container}>
+			<Text style={styles.title}>Date of Birth</Text>
+			<DateTimePicker
+				date={date}
+				onChange={params => params.date && setDate(dayjs(params.date))}
+				mode='single'
+			/>
+			<Button title='Calculate Age' onPress={calculateAge} />
 		</View>
 	);
 };
 
 export default AgeCalculator;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+
+		alignItems: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+
+		marginBottom: 20,
+	},
+});
